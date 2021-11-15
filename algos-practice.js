d93582fab71281c62c82f691255e0ca562984a39
Madhu Ayyagari <madhu@Veerashs-MacBook-Air.local>
2021-11-14 17:21:12 -0800

    Done with Counting Sort 1

--- algos-practice.js
+++ algos-practice.js
@@ -263,3 +263,31 @@ function diagonalDifference(arr) {
   let absTotal = Math.abs(total)
   return absTotal;
 }
+
+// 10.Counting Sort 1
+// Sample Input
+
+// 100
+// 63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 
+// 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79
+// 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22
+// 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82
+// 67 61 32 21 79 75 75 13 87 70 33
+// Sample Output
+
+// 0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 
+// 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1
+function countingSort(arr) {
+  // Write your code here
+  let freqCounter = {}
+
+
+  for(let num = 0; num < 100; num++){
+      freqCounter[num] = (freqCounter[num]++ || 0 ); 
+      }
+  for(let num of arr){
+      freqCounter[num] = (freqCounter[num] || 0) + 1
+  }
+  return (Object.values(freqCounter))
+
+}

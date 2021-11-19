b7c1d147b1d36df451905a699904c91c8c236f4c
Madhu Ayyagari <madhu@Veerashs-MacBook-Air.local>
2021-11-19 13:55:03 -0800

    Permuting two arrays

--- algos-practice.js
+++ algos-practice.js
@@ -328,3 +328,39 @@ function pangrams(s) {
   }
 
 }
+
+// 12. Permuting Two Arrays
+// Sample Input
+
+// STDIN       Function
+// -----       --------
+// 2           q = 2
+// 3 10        A[] and B[] size n = 3, k = 10
+// 2 1 3       A = [2, 1, 3]
+// 7 8 9       B = [7, 8, 9]
+// 4 5         A[] and B[] size n = 4, k = 5
+// 1 2 2 1     A = [1, 2, 2, 1]
+// 3 3 3 4     B = [3, 3, 3, 4]
+// Sample Output
+
+// YES
+// NO
+function twoArrays(k, A, B) {
+  // let letterA = A.sort((a, b) => a - b);
+  // let letterB = B.sort((a, b)=> b - a);
+  // return A.every((n, i)=> n + B[i] >= k)?"YES":"NO";
+  
+  
+  let letterA = A.sort((a, b) => a - b)
+  let letterB = B.sort((a, b)=> b - a);
+  let ayo;
+  for(let i = 0; i < letterA.length; i++){
+      if(letterA[i] + letterB[i] >= k){     
+      ayo = "YES"
+      }else {
+          return ayo = "NO"
+      }
+  }
+  return ayo
+}
+

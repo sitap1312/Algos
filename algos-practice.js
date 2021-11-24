ec80b4dded9da5575969b86fb73e97d892203fab
Madhu Ayyagari <madhu@Veerashs-MacBook-Air.local>
2021-11-23 16:03:36 -0800

    Done with Correct the mistakes of the character recognition software

--- algos-practice.js
+++ algos-practice.js
@@ -483,3 +483,16 @@ function makeNegative(num) {
   }
   
 }
+
+// 18. Correct the mistakes of the character recognition software
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+function correct(string)
+{
+	// your code here
+  return string.replace(/0/g, "O").replace(/1/g, "I").replace(/5/g, "S")
+}

fe9ca839fc641438082bc58017bce075eb6558d3
Madhu Ayyagari <madhu@Veerashs-MacBook-Air.local>
2021-12-12 15:30:31 -0800

    Done with Calculate BMI

--- algos-practice.js
+++ algos-practice.js
@@ -540,3 +540,17 @@ function isIsogram(str){
 //   }
   return !str.match(/([a-z]).*\1/i);
 }
+
+// 22. Calculate BMI
+function bmi(weight, height) {
+  let bmi1 = weight / (height * height)
+  if(bmi1 <= 18.5) {
+    return "Underweight"
+  } else if (bmi1 <= 25.0) {
+    return "Normal"
+  } else if (bmi1 <= 30.0) {
+    return "Overweight"
+  } else if (bmi1 > 30) {
+    return "Obese"
+  }
+}
